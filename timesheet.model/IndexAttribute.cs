eb02b3a23328e3b0178bfcc1871d064d5347a4d6
SunainaAseeb <sunaina.aseebyel@gmail.com>
2019-04-23 08:03:05 +0400

    Fixing compilation error for IndexAttribute

---
+++ timesheet.model/IndexAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace timesheet.model
+{
+    internal class IndexAttribute : Attribute
+    {
+        private string v;
+        private bool IsUnique;
+
+        public IndexAttribute(string v, bool IsUnique)
+        {
+            this.v = v;
+            this.IsUnique = IsUnique;
+        }
+    }
+}
